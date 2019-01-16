34c1e52ad37170490d3afda9022ddf5a58dc8c7a
Nata P <protasnb@yahoo.com>
2019-01-16 12:50:24 -0800

    Add user listing and show articles view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,25 +3,49 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def index
+        @users = User.all
+    end
+    
     def create
         @user = User.new(user_params)
         if @user.save
             # session[:user_id] = @user.id
             flash[:success] = "Welcome to the Beta Blog #{@user.username}!"
             # redirect_to user_path(@user)
-            redirect_to articles_path
+            redirect_to users_path
         else
             render 'new'
         end
     end
 
+    def update
+        @user = User.find(params[:id])
+
+        if @user.update(user_params)
+            flash[:success] = "Your account was successfully updated"
+            # redirect_to user_path(@user)
+            redirect_to users_path
+        else
+            flash[:danger] = "User was not updated"
+            redirect_to users_path
+        end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
     private
 
     def set_user
         @user = User.find(params[:id])
     end
 
-  def user_params
-    params.require(:user).permit(:username, :email, :password)
-  end
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
