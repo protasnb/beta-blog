e1000ba3cab58160f1a4e9aed45b90021ad2c2a1
Nata P <protasnb@yahoo.com>
2019-01-16 16:35:17 -0800

    Add sing in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,10 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            # session[:user_id] = @user.id
+            session[:user_id] = @user.id
             flash[:success] = "Welcome to the Beta Blog #{@user.username}!"
-            # redirect_to user_path(@user)
-            redirect_to users_path
+            redirect_to user_path(@user)
+
         else
             render 'new'
         end
